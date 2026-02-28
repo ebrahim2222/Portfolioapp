1c11d23a8733bdb0c08c479f685a2161037f5c83
Ebrahim El-Bitawy <elbitawy@gmail.com>
2026-02-28 05:55:34 +0300

    Use relative paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Portfolioapp",
+  base: "./",
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
